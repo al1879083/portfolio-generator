cf90fd7caefbef339dc572b698bd2e3208a90042
Adrian <adrianlandag@gmail.com>
2021-04-06 18:43:21 -0700

    added new add.js

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
+
 // Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i++) {
       console.log(profileDataArr[i]);
     }
   };
+
+let message = 'Hello Node!';
+message = 'Hello ES6!';
+
+let sum = 5 + 3;
+sum += 1;
+
+const message = 'Hello Node!';
+
+if (true === true) {
+  const message = 'Hello ES6!';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
+
+console.log(message);
+console.log(sum);
