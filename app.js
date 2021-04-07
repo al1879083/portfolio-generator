bd38261fc8e9e012641ed507d23ab077ce019fa6
Adrian <adrianlandag@gmail.com>
2021-04-06 17:28:54 -0700

    new js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log('Hello Node!');
-console.log(document); 
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
